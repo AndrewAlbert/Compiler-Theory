a48262db2bedabc9b9fbc6c04dc87bb005f86cab
Andrew Albert <AndrewAlbert@users.noreply.github.com>
2016-06-10 00:25:55 -0400

    Update scopeValue.h
    Changed parameter list to type scopeValue to make tracking which symbols correspond to which parameter easier in the parser.

--- scopeValue.h
+++ scopeValue.h
@@ -2,13 +2,15 @@
 #define SCOPEVALUE_H
 
 #include <vector>
+#include "scopeValue.h"
+
 using namespace std;
 
 struct scopeValue{
 	int type; //symbol type
 	int size; //size for arrays (0 for non arrays)
-	vector<int> arguments; //vector of input argument types
-	int paramType;
+	vector<scopeValue> arguments; //vector of input arguments
+	int paramType; //parameter type IN | OUT | INOUT | not a parameter
 };
 
 #endif
