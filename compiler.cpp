3fe194eec4a8327ab677d747384c4e1f7ba4691a
Andrew Albert <andrew.christian.albert@gmail.com>
2016-06-18 23:24:22 -0400

    updated compiler.cpp command line arguments

--- compiler.cpp
+++ compiler.cpp
@@ -3,8 +3,8 @@
 #include "macro.h"
 #include "scope.h"
 #include "scopeTracker.h"
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
 #include <cstdio>
 
 using namespace std;
@@ -17,24 +17,24 @@ void commandError(){
 //Program call should be: ./compiler [-help] filename
 int main(int argc, char **argv){
 	string filename;
-	if( (argc == 0) || (argc > 2) ){
-		commandError();
-		return 0;
-	}
-	else if( ( argv[0] == "--help") || ( argv[0] == "--h") ){
+	if( (argc == 2) || (argc == 3) ){
+		if( (string(argv[1]) == "--help") || (string(argv[1]) == "--h") ){
 		cout << "This is a compiler written for the University of Cincinnati class: EECE6083 Compiler Theory" << endl;
 		cout << "The compiler acts as an LL(1) recursive descent compiler and utilizes the C++ programming language and LLVM" << endl;
 		cout << "To use this compiler, compile and then run from the command line using the arguments: [ --help | --h ] filename" << endl;
 		cout << "The compiler will scan and parse your file and generate code if parsing is successful. Otherwise appropriate errors and warnings will be shown" << endl;
-	}
-	else if( argc == 1 ){
-		//test to see if the file can be read
-		ifstream inputfile(filename);
-		if( inputfile.is_open() ) inputfile.close();
-		else{
-			commandError();
-			return 0;
+		return 0;
 		}
+		else{
+			filename = string(argv[1]);
+			//test to see if the file can be read
+			ifstream inputfile(filename);
+			if( inputfile.is_open() ) inputfile.close();
+			else{
+				commandError();
+				return 0;
+			}
+		}	
 	}
 	else{
 		commandError();
