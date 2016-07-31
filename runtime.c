6dc689563877aa8e992507dfc8a98cb22165dfd3
Andrew Albert <andrew.christian.albert@gmail.com>
2016-07-31 03:23:41 -0400

    Add runtime support files.

---
+++ runtime.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void handleBool( int val ){
+	if( val != 0 && val != 1 ){
+		printf("Runtime Error: integer %d is not compatible with boolean values.\n", val);
+		exit(EXIT_FAILURE);
+	}
+	else return;
+}
+
+int getBool(){
+	int val;
+	scanf("%d", &val);
+	handleBool( val );
+	return val;
+}
+
+int getInteger(){
+	int val;
+	scanf("%d", &val);
+	return val;
+}
+
+float getFloat(){
+	float val;
+	scanf("%f", &val);
+	return val;
+}
+
+int getString( char* buffer ){
+	fgets(buffer, 256, stdin);
+	float val = (float)sizeof(char)/sizeof(int);
+	return ( val*(float)strlen(buffer) + 0.5 );
+}
+
+char getChar(){
+	char val;
+	scanf("%c", &val);
+	return val;
+}
+
+void putBool( int val ){
+	handleBool( val );
+	printf("%d", val);
+	return;
+}
+
+void putInteger( int val ){
+	printf("%d", val);
+	return;	
+}
+
+void putFloat( float val ){
+	printf("%f", val);
+	return;
+}
+
+void putString( char* str_ptr ){
+	printf("%s", str_ptr);
+	return; 
+}
+
+void putChar( char val ){
+	printf("%c", val);
+	return;
+}
